1600c88fc9d45f293472fa8f252cf0c2ce61a665
이담영 (Lee Damyeong) <yidamyeong@gmail.com>
2025-06-06 14:54:47 +0000

    fix: 환경변수 세팅 수정

--- poet.sh
+++ poet.sh
@@ -5,4 +5,4 @@ VERSE=$(shuf -n 1 verses.txt)
 echo "$(date '+%Y-%m-%d') - $VERSE" >> poem.log
 git add poem.log
 git commit -m "$VERSE"
-git push https://damyeong:${{ secrets.GH_PAT }}@github.com/damyeong/plant-a-commit.git HEAD:green
+git push https://damyeong:${GH_PAT}@github.com/damyeong/plant-a-commit.git HEAD:green
